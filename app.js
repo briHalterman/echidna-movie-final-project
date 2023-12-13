af5add7870cb5d043ffc7268acd31ca57d1dda57
Bri Halterman <halterma@gmail.com>
2023-12-13 10:49:51 -0500

    create start function

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 // listen on port 3000
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, console.log(`server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error);
+  } 
+};
