3b448dfcdcd52e96a7d59f2413c8c1c66ad1be77
Bri Halterman <halterma@gmail.com>
2023-12-13 10:46:27 -0500

    dynamicize port variable

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 // listen on port 3000
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}...`);
