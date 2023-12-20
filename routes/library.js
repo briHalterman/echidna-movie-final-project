862796f855d5e00d3088809b182ee5c9ab12a6e2
Bri Halterman <halterma@gmail.com>
2023-12-20 11:59:31 -0500

    set up library router

---
+++ routes/library.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getAllMovies,
+  getMovie,
+  createMovie,
+  updateMovie,
+  removeMovie
+} = require('../controllers/movies');
+
+router.route('/').post(createMovie).get(getAllMovies);
+router.route('/:id').get(getMovie).delete(removeMovie).patch(updateMovie);
+
+module.exports = router;
