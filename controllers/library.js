9546a27f007693114a6b7c880f5a1c510f4b33de
Bri Halterman <halterma@gmail.com>
2023-12-20 11:48:24 -0500

    set up library controllers

---
+++ controllers/library.js
@@ -0,0 +1,27 @@
+const getAllMovies = async (req, res) => {
+  res.send('get all movies');
+};
+
+const getMovie = async (req, res) => {
+  res.send('get individual movie');
+};
+
+const createMovie = async (req, res) => {
+  res.send('create movie');
+};
+
+const updateMovie = async (req, res) => {
+  res.send('update movie');
+};
+
+const removeMovie = async (req, res) => {
+  res.send('remove movie')
+};
+
+module.exports = {
+  getAllMovies,
+  getMovie,
+  createMovie,
+  updateMovie,
+  removeMovie
+};
