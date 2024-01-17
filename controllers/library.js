d9470582de4f447cc973c03919fcf44bc11c8e9d
Bri Halterman <halterma@gmail.com>
2024-01-17 14:44:07 -0500

    edit library controllers to incl 200 status and json msg

--- controllers/library.js
+++ controllers/library.js
@@ -6,23 +6,25 @@
 
 
 const getAllMovies = async (req, res) => {
-  // throw new Error('testing async errors');
   res.status(200).json({ msg: 'get all movies' });
 };
 
 const getMovie = async (req, res) => {
   res.status(200).json({ msg: 'get individual movie'});
+  res.status(200).json({ msg: 'get individual movie'});
 };
 
 const createMovie = async (req, res) => {
-  res.status(200).json(req.user);
+  res.status(200).json({ msg: 'create movie' });
 };
 
 const updateMovie = async (req, res) => {
   res.status(200).json({ msg: 'update movie' });
+  res.status(200).json({ msg: 'update movie' });
 };
 
 const removeMovie = async (req, res) => {
+  res.status(200).json({ msg: 'remove movie' });
   res.status(200).json({ msg: 'remove movie' });
 };
 
