6c9b99df0471dc1ab1e0c43ba3de07f6f154f27d
Bri Halterman <halterma@gmail.com>
2024-01-19 15:05:01 -0500

    create not found error and include in index

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,16 +1,16 @@
 // NOT FOUND ERROR
 
 // require status codes package
-const { StatusCodes } = require('http-status-codes');
+const { StatusCodes } = require("http-status-codes");
 // require custom API error
-const CustomAPIError = require('./custom-api');
+const CustomAPIError = require("./custom-api");
 
 class NotFoundError extends CustomAPIError {
   constructor(message) {
     super(message);
     this.statusCode = StatusCodes.NOT_FOUND;
-  };
-};
+  }
+}
 
 // export not found error
 module.exports = NotFoundError;
