cd889cb1aa624e9f64bb97d8d6351039972681b6
Bri Halterman <halterma@gmail.com>
2024-01-26 14:23:25 -0500

    develop user model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String
+  username: {
+    type: String,
+    required: [true, 'Please provide username'],
+    minlength: 4,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, // email validation regex
+      'Please provide valid email'
+    ],
+    unique
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 8,
+    maxlength: 12
+  } // we will remove maxlength later, once we hash our password
 });
 
 module.exports = mongoose.model('User', UserSchema);
