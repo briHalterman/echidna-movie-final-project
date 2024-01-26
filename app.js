85fb6490fbdefea733ba369d08b806c8450fd951
Bri Halterman <halterma@gmail.com>
2024-01-26 13:34:54 -0500

    reorganize app.js and add commentary

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 // require dotenv
 require('dotenv').config();
+// require express-async-errors
 require('express-async-errors');
-
-// extra security packages
-
 // require and invoke express
 const express = require('express');
 const app = express();
@@ -11,26 +9,30 @@ const app = express();
 // connect DB
 const connectDB = require('./db/connect');
 
-const notFound = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-
 // routers
 const authRouter = require('./routes/auth');
 const libraryRouter = require('./routes/library');
 
+// error handlers
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 // configure express.json()
 app.use(express.json());
 
+// extra security packages
+
+
 // routes
-// route for handling get request for path /
-// app.get('/', (req, res) => {
-//   res.send('movie library api');
-// });
+  // route for handling get request for path /
+  // app.get('/', (req, res) => {
+  //   res.send('movie library api');
+  // });
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/library', libraryRouter);
 
-// error handling
-app.use(notFound);
+// invoke error handling
+app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 // invoke extra packages
@@ -40,7 +42,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI) // connectDB
+    await connectDB(process.env.MONGO_URI) // connect to DB
     app.listen(port, console.log(`server is listening on port ${port}...`))
   } catch (error) {
     console.log(error);
