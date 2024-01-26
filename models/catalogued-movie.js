f057aecfc53b1f6eec7d1e0bc42341cfbaddcee8
Bri Halterman <halterma@gmail.com>
2024-01-26 15:10:01 -0500

    develop catalogued movie model

--- models/catalogued-movie.js
+++ models/catalogued-movie.js
@@ -2,7 +2,22 @@
 const mongoose = require('mongoose');
 
 const CataloguedMovieSchema = new mongoose.Schema({
-
+  title: {
+    type: String,
+    required: [true, 'Please provide title'],
+    maxlength: 100
+  },
+  director: {
+    type: String,
+    maxlength: 100
+  },
+  year: {
+    type: Number, // date?
+    min: 1888,
+    // current year + 10 in YYYY form
+    max: 2034 
+    // var maxYear = currentTime.getFullYear() + 10
+  }
 });
 
 module.exports = mongoose.model('catalogued-movie', CataloguedMovieSchema);
