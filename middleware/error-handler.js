63b9eacdac26a1d1d24761e08e05056fdecb4fb5
Bri Halterman <halterma@gmail.com>
2024-02-29 19:36:00 -0500

    handle duplicate email error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,16 +1,29 @@
 // ERROR HANDLER MIDDLEWARE
 
 // import custom API error (from errors)
-const { CustomAPIError } = require("../errors");
+// const { CustomAPIError } = require("../errors");
 // require status codes package
 const { StatusCodes } = require("http-status-codes");
 
 // error handler middleware
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later'
+  };
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.code && err.code == 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 };
 
 // export error handler middleware
